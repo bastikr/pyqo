cd6638d3c20bdf15c25b98f343269afb7865b12a
Sebastian Kraemer <basti.kr@gmail.com>
2011-12-05 21:50:22 +0100

    Change dynamic operator class.
    Not yet usable...

--- pyqo/dynamic_operators.py
+++ pyqo/dynamic_operators.py
@@ -1,6 +1,8 @@
 from . import operators
 
 class DynamicOperator(operators.BaseOperator):
-    def __init__(self, func, basis=None):
+    def __init__(self, func, t_min, t_max, basis=None):
         self.basis = basis
         self.__call__ = func
+        self.t_min = t_min
+        self.t_max = t_max
