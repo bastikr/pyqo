2fc2798590e8c1d350f1e062042943aa163d7952
Sebastian Kraemer <basti.kr@gmail.com>
2011-12-06 17:49:01 +0100

    Add calculation of coherent state in number basis.

--- pyqo/bases/__init__.py
+++ pyqo/bases/__init__.py
@@ -1,2 +1,4 @@
 from .basis import Basis, ONBasis, CompositeBasis, compose_bases
 from .coherent_basis import CoherentBasis
+from . import coherent_basis
+from . import number_basis
---
+++ pyqo/bases/number_basis.py
@@ -0,0 +1,10 @@
+import numpy
+
+def coherent_state(alpha, N, dtype=complex):
+    from .. import statevector
+    x = numpy.empty(N, dtype=dtype)
+    x[0] = 1
+    for n in range(1,N):
+        x[n] = x[n-1]*alpha/numpy.sqrt(n)
+        x = numpy.exp(-numpy.abs(alpha)**2/2.)*x
+    return statevector.StateVector(x)
