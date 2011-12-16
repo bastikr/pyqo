478a6d5c4f6f1397a4f71b789fd746a9e9790de6
Sebastian Kraemer <basti.kr@gmail.com>
2011-12-16 15:12:56 +0100

    Fix bug in calc. of coh. states in number basis

--- pyqo/bases/number_basis.py
+++ pyqo/bases/number_basis.py
@@ -6,5 +6,5 @@ def coherent_state(alpha, N, dtype=complex):
     x[0] = 1
     for n in range(1,N):
         x[n] = x[n-1]*alpha/numpy.sqrt(n)
-        x = numpy.exp(-numpy.abs(alpha)**2/2.)*x
+    x = numpy.exp(-numpy.abs(alpha)**2/2.)*x
     return statevector.StateVector(x)
