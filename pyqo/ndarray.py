e11c2d1cb487f6bac19175e8b739388549ee6c59
Sebastian Kraemer <basti.kr@gmail.com>
2011-12-16 15:11:58 +0100

    Improve basic Array class to work better with mp

--- pyqo/ndarray.py
+++ pyqo/ndarray.py
@@ -3,20 +3,18 @@ import numpy
 from . import datatypes
 from . import utils
 
-DEFAULT_DTYPE = [complex]
 
 class Array(numpy.ndarray):
     def __new__(cls, data, basis=None, **kwargs):
-        #if "dtype" not in kwargs:
-        #    print("no dtype")
-        #    kwargs["dtype"] = DEFAULT_DTYPE[0]
-        #print("__new__ dtype:", kwargs["dtype"])
         array = numpy.array(data, **kwargs)
+        """
         if array.dtype in (numpy.float16, numpy.float32,
                                     numpy.float64, numpy.float128):
             kwargs["dtype"] = complex
             array = numpy.array(data, **kwargs)
         array = numpy.asarray(array).view(cls)
+        """
+        array = array.view(cls)
         array.basis = basis
         cls._check(array)
         return array
@@ -55,3 +53,67 @@ class Array(numpy.ndarray):
             return self._real()
         else:
             return numpy.ndarray.real.__get__(self)
+
+    def __mul__(self, other):
+        result = (self.view(numpy.ndarray) * other)
+        if isinstance(result, numpy.ndarray):
+            result = result.view(self.__class__)
+            result.basis = self.basis
+        return result
+
+    def __rmul__(self, other):
+        result = (other * self.view(numpy.ndarray))
+        if isinstance(result, numpy.ndarray):
+            result = result.view(self.__class__)
+            result.basis = self.basis
+        result.basis = self.basis
+        return result
+
+    def __div__(self, other):
+        result = (self.view(numpy.ndarray) / other)
+        if isinstance(result, numpy.ndarray):
+            result = result.view(self.__class__)
+            result.basis = self.basis
+        return result
+
+    def __rdiv__(self, other):
+        result = (other / self.view(numpy.ndarray))
+        if isinstance(result, numpy.ndarray):
+            result = result.view(self.__class__)
+            result.basis = self.basis
+        result.basis = self.basis
+        return result
+
+
+    def __add__(self, other):
+        result = (self.view(numpy.ndarray) + other)
+        if isinstance(result, numpy.ndarray):
+            result = result.view(self.__class__)
+            result.basis = self.basis
+        result.basis = self.basis
+        return result
+
+    def __radd__(self, other):
+        result = (other + self.view(numpy.ndarray))
+        if isinstance(result, numpy.ndarray):
+            result = result.view(self.__class__)
+            result.basis = self.basis
+        result.basis = self.basis
+        return result
+
+    def __sub__(self, other):
+        result = (self.view(numpy.ndarray) - other)
+        if isinstance(result, numpy.ndarray):
+            result = result.view(self.__class__)
+            result.basis = self.basis
+        result.basis = self.basis
+        return result
+
+    def __rsub__(self, other):
+        result = (other - self.view(numpy.ndarray))
+        if isinstance(result, numpy.ndarray):
+            result = result.view(self.__class__)
+            result.basis = self.basis
+        result.basis = self.basis
+        return result
+
