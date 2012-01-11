fa28cdcdcdcba3e3cc9148f4564b42a7389925d3
Sebastian Kraemer <basti.kr@gmail.com>
2012-01-11 10:50:07 +0100

    Some changes to single atom laser.

--- examples/single_atom_laser.py
+++ examples/single_atom_laser.py
@@ -8,7 +8,7 @@ delta_a = 1
 g = 1
 gamma = 0.1
 kappa = 0.1
-R = 0.5
+R = 0.2
 
 # Field
 id_f = qo.identity(N)
@@ -20,7 +20,7 @@ n = qo.number(N)
 id_a = qo.identity(2)
 
 # Initial state
-psi_0 = qo.basis_vector(N,0) ^ qo.basis_vector(2,1)
+psi_0 = qo.basis_vector(N,3) ^ qo.basis_vector(2,1)
 
 # Hamiltonian
 H = delta_c*(at*a^id_a) + delta_a*(id_f^qo.sigmap*qo.sigmam) + g*(a^qo.sigmap) + g*(at^qo.sigmam)
@@ -32,30 +32,17 @@ j3 = R**(1./2)*(id_f^qo.sigmap)
 J = [j1, j2, j3]
 
 # Solve Master equation
-T = np.linspace(0, 3*np.pi, 20)
-rho = qo.solve_ode(H, psi_0, T, J)
-#rho = qo.solve_mc_single(H, psi_0, T, J, seed=0)
+T = np.linspace(0, 5*np.pi, 100)
+#rho = qo.solve_ode(H, psi_0, T, J)
+rho = qo.solve_mc_single(H, psi_0, T, J, seed=0)
 #rho = qo.solve_es(H, psi_0, T, J)
 
 # Expectation values
 n_exp, e_exp = qo.expect((n^id_a, id_f^qo.sigmap*qo.sigmam), rho)
 
-# Calculate Q-function and photon number distribution
-x_min, x_max = -5, 5
-y_min, y_max = -5, 5
-x = np.linspace(x_min, x_max, 30)
-y = np.linspace(y_min, y_max, 30)
-X, Y = np.meshgrid(x,y)
-
-Q = []
-F = []
-for rho_t in rho:
-    rho_f = rho_t.ptrace(1)
-    Q.append(np.abs(qo.qfunc(rho_f,X,Y)))
-    F.append(np.abs(np.diag(rho_f)))
-
 # Visualization
 import pylab
+#"""
 pylab.figure(1)
 pylab.subplot(211)
 pylab.xlabel("time")
@@ -69,8 +56,22 @@ pylab.ylim((0,1))
 pylab.plot(T, np.abs(e_exp))
 pylab.show()
 
-from scipy.misc import factorial
+"""
+# Calculate Q-function and photon number distribution
+x_min, x_max = -4, 4
+y_min, y_max = -4, 4
+x = np.linspace(x_min, x_max, 40)
+y = np.linspace(y_min, y_max, 40)
+X, Y = np.meshgrid(x,y)
 
+Q = []
+F = []
+for rho_t in rho:
+    rho_f = rho_t.ptrace(1)
+    Q.append(np.abs(qo.qfunc(rho_f,X,Y)))
+    F.append(np.abs(np.diag(rho_f)))
+
+from scipy.misc import factorial
 F_x = np.arange(0, N)
 def fplot(fig, step):
     axes = fig.add_subplot(111)
@@ -87,3 +88,4 @@ def qplot(fig,step):
                 extent=(x_min, x_max, y_min, y_max))
 
 qo.animate(len(rho), qplot)
+#"""
