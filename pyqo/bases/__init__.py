dd3404353e666a33497e34ce37d7b28f83a826f4
Sebastian Kraemer <basti.kr@gmail.com>
2012-01-11 10:28:00 +0100

    Implement number bases.

--- pyqo/bases/__init__.py
+++ pyqo/bases/__init__.py
@@ -1,4 +1,5 @@
 from .basis import Basis, ONBasis, CompositeBasis, compose_bases
+from .number_basis import NumberBasis
 from .coherent_basis import CoherentBasis
 from . import coherent_basis
 from . import number_basis
--- pyqo/bases/number_basis.py
+++ pyqo/bases/number_basis.py
@@ -1,5 +1,55 @@
 import numpy
 
+from . import basis
+
+def part_factorial(X, n):
+    if n==0:
+        return X**0
+    return X*part_factorial(X-1, n-1)
+
+class NumberBasis(basis.ONBasis):
+    rank = 1
+    N0 = None
+    N1 = None
+    dtype = None
+
+    def __init__(self, N0, N1=None, dtype=None):
+        if N1 is None:
+            self.N0 = 0
+            self.N1 = N0
+        else:
+            self.N0 = N0
+            self.N1 = N1
+        self.dtype = dtype
+
+    def basis_vector(self, i):
+        from ..statevector import StateVector
+        assert self.N0 <= i < self.N1
+        if self.dtype is None:
+            X = numpy.zeros(self.N1-self.N0)
+            X[i-self.N0] = 1
+        else:
+            X = numpy.empty(self.N1-self.N0, dtype=self.dtype)
+            for j in range(0, len(X)):
+                X[j] = self.dtype(0)
+            X[i-self.N0] = self.dtype(1)
+        return StateVector(X, basis=self)
+
+    def destroy(self, pow=1):
+        from ..operators import Operator as op
+        N0 = self.N0 + pow
+        N1 = self.N1
+        if self.dtype is None:
+            X = numpy.arange(N0, N1)
+        else:
+            X = numpy.array(list(self.dtype(i) for i in range(N0,N1)))
+        X = part_factorial(X, pow)**(0.5)
+        return op(numpy.diag(X, pow), basis=self)
+
+    def create(self, pow=1):
+        return self.destroy(pow=pow).T
+
+
 def coherent_state(alpha, N, dtype=complex):
     from .. import statevector
     x = numpy.empty(N, dtype=dtype)
