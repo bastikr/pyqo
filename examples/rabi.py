9c31f1792b4ed4b96ef23edc7189e95a45e04366
Sebastian Kraemer <basti.kr@gmail.com>
2012-01-12 14:01:37 +0100

    Small fix to "rabi" example.

--- examples/rabi.py
+++ examples/rabi.py
@@ -14,13 +14,14 @@ psi_0 = qo.basis_vector(2,0)
 
 T = np.linspace(0,2*np.pi,30)
 #psi = qo.solve_ode(H, psi_0, T)
+#psi = qo.solve_master(H, psi_0, T)
 psi = qo.solve_mc_single(H, psi_0, T)
 
 e0 = qo.expect(qo.sigmap*qo.sigmam, psi)
 e1 = qo.expect(qo.sigmam*qo.sigmap, psi)
 
 import pylab
-pylab.plot(T, e0, T, e1)
+pylab.plot(T, np.real(e0), T, np.real(e1))
 pylab.xlabel("Time")
 pylab.ylabel("Occupation")
 pylab.show()
