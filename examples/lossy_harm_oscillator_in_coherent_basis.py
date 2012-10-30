5ea71aa2b6451cb5c116d20e9787e4c3f806ceb3
Sebastian Kraemer <basti.kr@gmail.com>
2012-10-30 16:51:17 +0100

    Small changes in one example.

--- examples/lossy_harm_oscillator_in_coherent_basis.py
+++ examples/lossy_harm_oscillator_in_coherent_basis.py
@@ -4,23 +4,25 @@ import numpy as np
 import mpmath as mp
 import pylab
 
-mp.mp.dps = 64
+mp.mp.dps = 128
 
 # Parameter of system
 omega = mp.mpf("1")
-eta = mp.mpf("0.2")
+kappa = mp.mpf("0")
+eta = mp.mpf("-5")
 
 # Choose initial coherent basis and state
-basis = qo.bases.CoherentBasis.create_hexagonal_grid_rings(2, 1.3, 3)
+basis = qo.bases.CoherentBasis.create_hexagonal_grid_nearestN(2.1, 0.7, 2.1, 28)
 basis = basis.dual_basis(bra=True, ket=False)
-psi_0 = basis.coherent_state(mp.mpf("2.1"))
+psi_0 = basis.coherent_state(mp.mpf("2"))
 
 # Define Hamiltonian (dependent on basis)
 def create_H(t, basis):
-    return omega*basis.create_destroy(1,1)
+    return omega*basis.create_destroy(1,1) +\
+           eta*(basis.create() + basis.destroy())
 
 def create_J(t, basis):
-    return [eta*basis.destroy()]
+    return [kappa*basis.destroy()]
 
 # Hamiltonian for initial basis
 H_0 = create_H(0, basis)
@@ -30,22 +32,24 @@ J_0 = create_J(0, basis)
 AM = qo.adaptive.AM_Coherent(create_H, create_J)
 
 # Solve master equation
-T = np.linspace(0, np.pi/2, 6)
+T = np.linspace(0, 2*np.pi, 6)
 #T = np.linspace(0, 0.2, 6)
 psi_t = qo.solve_mc_single(H_0, psi_0, T, J_0, adapt=AM)
 
 # Visualization
 exp_a = []
 
-X,Y = np.meshgrid(np.linspace(-6,6), np.linspace(-6,6))
+X,Y = np.meshgrid(np.linspace(0,12), np.linspace(0,12))
+
 log = np.vectorize(mp.log)
 print len(psi_t)
 for i, psi in enumerate(psi_t):
     pylab.subplot(2,3,i+1)
     b = psi.basis
     # Calculate <a>
-    a = psi.basis.destroy()
-    exp_a.append(qo.expect(a, psi))
+    a = b.destroy()
+    exp_a.append(np.dot(psi.dual().conj(), a*psi))
+
     exp_a_numpy = np.array(exp_a, dtype=complex)
 
     # Calculate Q-function
@@ -57,8 +61,14 @@ for i, psi in enumerate(psi_t):
     z = np.array(log(np.abs(psi.dual().conj()*psi)), dtype=float)
 
     pylab.scatter(states.real, states.imag, c=z)
-    pylab.imshow(Q_numpy, origin="lower", extent=(-6,6,-6,6))
+    pylab.imshow(Q_numpy, origin="lower", extent=(0,12,0,12))
     pylab.contour(X,Y,Q)
     pylab.plot(exp_a_numpy.real, exp_a_numpy.imag)
+    sp0 = b.coherent_scalar_product(mp.mpf("2"), b.states)
+    sp1 = b.coherent_scalar_product(mp.conj(exp_a[-1]), b.states)
+    error_sv0 = 2*(1-(np.dot(sp0, psi)).real)
+    error_sv1 = 2*(1-(np.dot(sp1, psi)).real)
+    print(error_sv0, error_sv1)
 
+print(exp_a_numpy)
 pylab.show()
