0764728271a8f99cdb88867f753a194e89fdc5dd
Sebastian Krämer <basti.kr@gmail.com>
2013-07-03 18:58:08 +0200

    Improve io.py

--- pyqo/io.py
+++ pyqo/io.py
@@ -1,5 +1,37 @@
+import os
 import pyqo
 import numpy
+
+def guess_type(x):
+    try:
+        return int(x)
+    except ValueError:
+        pass
+    try:
+        return float(x)
+    except ValueError:
+        pass
+    return x
+
+def filename2dict(path, types=None):
+    directory, name = os.path.split(path)
+    assert name
+    if name.endswith(".npz"):
+        name = name[:-4]
+    d = {}
+    for pair in name.split(";"):
+        key, value = pair.split("=")
+        if types is None:
+            value = guess_type(value)
+        else:
+            value = types[key](value)
+        d[key] = value
+    return d
+
+def dict2filename(d):
+    keys = d.keys()
+    keys.sort()
+    return ";".join(str(k)+"="+str(d[k]) for k in keys)
 
 def save(path, obj):
     f = open(path, "w")
